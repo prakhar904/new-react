15f4e880947fb3fb22b10f783542fd160e59e4f3
Prakhar Garg <gargprakhar93@gmail.com>
2020-06-12 03:26:18 +0530

    new

---
+++ public/precache-manifest.c2016597d782bf37e20b7d239cc587c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba35ce3d931f17bd2f8b4c02d815588",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e900ea404db9cddc60",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "1d32a819551e2e44b3c8",
+    "url": "/static/css/main.e14550cb.chunk.css"
+  },
+  {
+    "revision": "46e900ea404db9cddc60",
+    "url": "/static/js/2.de120fb6.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.de120fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d32a819551e2e44b3c8",
+    "url": "/static/js/main.6e421fc1.chunk.js"
+  },
+  {
+    "revision": "79298229a104881fd483",
+    "url": "/static/js/runtime-main.ad013ee6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
